53627e1d8328a56380fc6185822759decb983384
Bluepaper <alirezagh110@gmail.com>
2019-01-04 11:10:52 +0330

    MainActivity for start game and connect3 activity for game created

---
+++ app/src/main/java/e/bluepaper/connect3/MainActivity.java
@@ -0,0 +1,20 @@
+package e.bluepaper.connect3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void startGame(View v){
+        if (v.getId() == R.id.btn_start){
+            new Intent(MainActivity.this, ConnectThree.class);
+        }
+    }
+}
